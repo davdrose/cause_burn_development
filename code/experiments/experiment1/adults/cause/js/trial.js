d0c7af081fad2bde02f8e7a2b5fbb82b29a977a1
David Rose <davdrose@stanford.edu>
2024-09-06 15:43:10 -0400

    added exps

---
+++ code/experiments/experiment1/adults/cause/js/trial.js
@@ -0,0 +1,76 @@
+
+// const trials = {
+//     "instructions": ["instructions.png"],
+//     "next_scenario": ["next_scenario.png"],
+//     "break": {
+//         "boy_distal": {
+//             "story": ["break_boy_distal_intro1.png", "break_boy_distal_intro2.png", "break_boy_distal_intro3.png", "break_boy_distal_intro4.png", "break_boy_distal_intro5.png", "break_boy_distal_intro6.png", "break_boy_distal_intro1.png"], 
+//             "questions": {
+//                 "cause": "break_boy_distal_cause.png",
+//                 "simple": "break_boy_distal_simple.png"
+//             },
+//         },
+//         "girl_distal": {
+//             "story": ["break_girl_distal_intro1.png", "break_girl_distal_intro2.png", "break_girl_distal_intro3.png", "break_girl_distal_intro4.png", "break_girl_distal_intro5.png", "break_girl_distal_intro6.png", "break_girl_distal_intro1.png"], 
+//             "questions": {
+//                 "cause": "break_girl_distal_cause.png",
+//                 "simple": "break_girl_distal_simple.png"
+//             },
+//         }, 
+//     },
+//     "crack": {
+//         "boy_distal": {
+//             "story": ["crack_boy_distal_intro1.png", "crack_boy_distal_intro2.png", "crack_boy_distal_intro3.png", "crack_boy_distal_intro4.png", "crack_boy_distal_intro5.png", "crack_boy_distal_intro6.png", "crack_boy_distal_intro7.png", "crack_boy_distal_intro8.png", "crack_boy_distal_intro9.png", "crack_boy_distal_intro10.png", "crack_boy_distal_intro11.png", "crack_boy_distal_intro12.png", "crack_boy_distal_intro13.png", "crack_boy_distal_intro14.png", "crack_boy_distal_intro15.png"],
+//             "questions": {
+//                 "cause": "crack_boy_distal_cause.png",
+//                 "simple": "crack_boy_distal_simple.png"
+//         },
+//         "girl_distal": {
+//             "story": ["crack_girl_distal_intro1.png", "crack_girl_distal_intro2.png", "crack_girl_distal_intro3.png", "crack_girl_distal_intro4.png", "crack_girl_distal_intro5.png", "crack_girl_distal_intro6.png", "crack_girl_distal_intro7.png", "crack_girl_distal_intro8.png", "crack_girl_distal_intro9.png", "crack_girl_distal_intro10.png", "crack_girl_distal_intro11.png", "crack_girl_distal_intro12.png", "crack_girl_distal_intro13.png", "crack_girl_distal_intro14.png", "crack_girl_distal_intro15.png"],
+//             "questions": {
+//                 "cause": "crack_girl_distal_cause.png",
+//                 "simple": "crack_girl_distal_simple.png"
+//             },
+//         },
+//     },
+// },
+// }
+
+
+
+const trials = {
+    "instructions": ["instructions.png"],
+    "next_scenario": ["next_scenario.png"],
+    "order_1": {
+        "break": {
+            "story": ["break_boy_distal_intro1.png", "break_boy_distal_intro2.png", "break_boy_distal_intro3.png", "break_boy_distal_intro4.png", "break_boy_distal_intro5.png", "break_boy_distal_intro6.png", "break_boy_distal_intro7.png", "break_boy_distal_intro8.png", "break_boy_distal_intro9.png", "break_boy_distal_intro10.png"],
+            "questions": {
+                "cause": "break_boy_distal_cause.png",
+                "simple": "break_boy_distal_simple.png"
+            },
+        },
+        "crack": {
+            "story": ["crack_girl_distal_intro1.png", "crack_girl_distal_intro2.png", "crack_girl_distal_intro3.png", "crack_girl_distal_intro4.png", "crack_girl_distal_intro5.png", "crack_girl_distal_intro6.png", "crack_girl_distal_intro7.png", "crack_girl_distal_intro8.png", "crack_girl_distal_intro9.png", "crack_girl_distal_intro10.png", "crack_girl_distal_intro11.png", "crack_girl_distal_intro12.png", "crack_girl_distal_intro13.png", "crack_girl_distal_intro14.png", "crack_girl_distal_intro15.png"],
+            "questions": {
+                "cause": "crack_girl_distal_cause.png",
+                "simple": "crack_girl_distal_simple.png"
+            },
+        },
+    },
+    "order_2": {
+        "break": {
+            "story": ["break_girl_distal_intro1.png", "break_girl_distal_intro2.png", "break_girl_distal_intro3.png", "break_girl_distal_intro4.png", "break_girl_distal_intro5.png", "break_girl_distal_intro6.png", "break_girl_distal_intro7.png", "break_girl_distal_intro8.png", "break_girl_distal_intro9.png", "break_girl_distal_intro10.png"],
+            "questions": {
+                "cause": "break_girl_distal_cause.png",
+                "simple": "break_girl_distal_simple.png"
+            },
+        },
+        "crack": {
+            "story": ["crack_boy_distal_intro1.png", "crack_boy_distal_intro2.png", "crack_boy_distal_intro3.png", "crack_boy_distal_intro4.png", "crack_boy_distal_intro5.png", "crack_boy_distal_intro6.png", "crack_boy_distal_intro7.png", "crack_boy_distal_intro8.png", "crack_boy_distal_intro9.png", "crack_boy_distal_intro10.png", "crack_boy_distal_intro11.png", "crack_boy_distal_intro12.png", "crack_boy_distal_intro13.png", "crack_boy_distal_intro14.png", "crack_boy_distal_intro15.png"],
+            "questions": {
+                "cause": "crack_boy_distal_cause.png",
+                "simple": "crack_boy_distal_simple.png"
+            },
+        },
+    },
+}
